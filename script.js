bc5e9b89918105b60b489b719c418c89d203f935
asekhon11 <101844469+asekhon11@users.noreply.github.com>
2023-12-05 22:03:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById('blogForm').addEventListener('submit', function(e) {
     console.log("Submitting blog content:", blogContent); // Log the blog content being submitted
 
     // Use the ngrok URL for the backend service
-    const ngrokURL = 'https://4408-99-246-144-174.ngrok-free.app/submit';
+    const ngrokURL = 'https://7324-99-246-144-174.ngrok-free.app/submit-blog';
 
     // Send the blog content to the backend using a POST request
     fetch(ngrokURL, {
@@ -28,5 +28,35 @@ document.getElementById('blogForm').addEventListener('submit', function(e) {
     .catch(error => {
         console.error('Error:', error);
         alert('Failed to submit blog. Check console for details.');
+    });
+});
+
+document.getElementById('tiktokForm').addEventListener('submit', function(e) {
+    e.preventDefault(); // Prevent the default form submission behavior
+
+    const tiktokURL = this.tiktokURL.value;
+    console.log("Submitting TikTok URL:", tiktokURL);
+
+    const ngrokURL = 'https://7324-99-246-144-174.ngrok-free.app/submit-tiktok';
+
+    fetch(ngrokURL, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({ url: tiktokURL })
+    })
+    .then(response => {
+        console.log("Response received:", response);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log("Data received:", data);
+        alert('TikTok transcribed successfully!');
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('Failed to transcribe TikTok. Check console for details.');
     });
 });
